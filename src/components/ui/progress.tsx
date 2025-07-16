2f371071097408253e34935e43f6431ac0c6917a
Molten Blox <yomaster880@gmail.com>
2025-07-16 04:27:28 +0000

    No use a black color please.

--- src/components/ui/progress.tsx
+++ src/components/ui/progress.tsx
@@ -12,7 +12,7 @@ const Progress = React.forwardRef<
   <ProgressPrimitive.Root
     ref={ref}
     className={cn(
-      "relative h-4 w-full overflow-hidden rounded-full bg-muted/30",
+      "relative h-4 w-full overflow-hidden rounded-full bg-muted",
       className
     )}
     {...props}
