b2a1b61d0351fda506864416477f415a6253033f
Molten Blox <yomaster880@gmail.com>
2025-07-16 04:22:36 +0000

    just remove the pattern from the lesson container

--- src/components/lesson/lesson-container.tsx
+++ src/components/lesson/lesson-container.tsx
@@ -81,24 +81,6 @@ export function LessonContainer({
 
   return (
     <div className="relative flex flex-col h-screen overflow-hidden font-body">
-       {/* Topography SVG background pattern */}
-        <div 
-            className="absolute inset-0 w-full h-full bg-primary opacity-15"
-            style={{
-              maskImage: 'url(/images/topography.svg)',
-              maskRepeat: 'repeat',
-              maskSize: 'auto',
-              WebkitMaskImage: 'url(/images/topography.svg)',
-              WebkitMaskRepeat: 'repeat',
-              WebkitMaskSize: 'auto',
-            }}
-        />
-        {/* Radial gradient to make the pattern transparent in the center */}
-        <div className="absolute inset-0 bg-background" style={{
-            maskImage: 'radial-gradient(ellipse at center, transparent 60%, black 95%)',
-            WebkitMaskImage: 'radial-gradient(ellipse at center, transparent 60%, black 95%)',
-        }}></div>
-
         {/* Glowing aura elements */}
         <div className="absolute top-0 left-1/4 h-96 w-96 bg-primary/10 blur-[150px] rounded-full -translate-x-1/2 -translate-y-1/2"></div>
         <div className="absolute top-0 right-1/4 h-80 w-80 bg-primary/10 blur-[120px] rounded-full translate-x-1/2 -translate-y-1/2"></div>
